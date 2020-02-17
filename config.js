a0e0227b4c1e6ed1b5643cd53333c50c12c7cf12
Burke Holland <burkeholland@gmail.com>
2020-02-17 15:34:04 -0600

    Update config

--- config.js
+++ config.js
@@ -1,11 +1,8 @@
 // @ts-check
 
 const config = {
-  // endpoint: "<Your Azure Cosmos account URI>",
-  // key: "<Your Azure Cosmos account key>",
-  endpoint: "https://fb2c4ff3-0ee0-4-231-b9ee.documents.azure.com:443/",
-  key:
-    "rgQwJf2ulYjgksTifH6l5MSj0VJlMMsWghBJsQM0Yw5IJVcSycZw3LTiL68MrGxtaVh2ekPEDWGlQvaT3hdgNg==",
+  endpoint: "<Your Azure Cosmos account URI>",
+  key: "<Your Azure Cosmos account key>",
   databaseId: "Tasks",
   containerId: "Items",
   partitionKey: { kind: "Hash", paths: ["/category"] }
